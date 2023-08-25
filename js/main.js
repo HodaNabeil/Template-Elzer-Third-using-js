b1f8e1477629aee00d32aba4e319331445963f1c
Hoda Nabeil <hodanabeil67@gmail.com>
2023-08-25 06:02:54 +0300

    Media style.css

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
-let section = document.querySelector(".our-price");
-let percentspans = document.querySelectorAll(".percent span");
-/////////////////
-let statsContainer = document.querySelector(".stats");
-let spansStats = document.querySelectorAll(" .conatainer-stats .box  span");
-let started = false; // Function Started ? No);
+let sectionPrice = document.querySelector(".our-price");
+let percentSpans = document.querySelectorAll(".percent span");
 
+//Price
 window.onscroll = function () {
-    if (window.scrollY >= section.offsetTop) {
-        percentspans.forEach((span) => {
+    if (window.scrollY >= sectionPrice.offsetTop) {
+        console.log("hoda");
+        percentSpans.forEach((span) => {
             span.style.width = span.dataset.width;
         });
     }
 };
-
 // =================statsContainer==================================
-
-window.onscroll = function () {
-    if (window.scrollY >= statsContainer.offsetTop) {
-        if (!started) {
-            spansStats.forEach((num) => startCount(num));
-        }
-        started = true;
-    }
-};
+// let statsContainer = document.querySelector(".stats");
+// let spansStats = document.querySelectorAll(" .conatainer-stats .box  span");
+// let started = false; // Function Started ? No;
+// window.onscroll = function () {
+//     if (window.scrollY >= statsContainer.offsetTop) {
+//         if (!started) {
+//             spansStats.forEach((num) => startCount(num));
+//         }
+//         started = true;
+//     }
+// };
 
 function startCount(el) {
     let goal = el.dataset.goal;
