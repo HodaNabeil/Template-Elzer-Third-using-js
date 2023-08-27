d59f690cfabe2e01f36d218d0250338149439b73
Hoda Nabeil <hodanabeil67@gmail.com>
2023-08-27 03:07:20 +0300

    add js in project

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
 let sectionPrice = document.querySelector(".our-price");
 let percentSpans = document.querySelectorAll(".percent span");
 
-//Price
+// =================statsContainer==================================
+let sectionStats = document.querySelector(".stats");
+let spanStats = document.querySelectorAll(".stats .conatainer-stats .box  span");
+let started = false; // Function Started ? No;
 window.onscroll = function () {
-    if (window.scrollY >= sectionPrice.offsetTop) {
-        console.log("hoda");
-        percentSpans.forEach((span) => {
-            span.style.width = span.dataset.width;
-        });
+    if (window.scrollY >= sectionStats.offsetTop) {
+        if (!started) {
+            spanStats.forEach((span) => startCount(span));
+        }
+        started = true;
     }
 };
-// =================statsContainer==================================
-// let statsContainer = document.querySelector(".stats");
-// let spansStats = document.querySelectorAll(" .conatainer-stats .box  span");
-// let started = false; // Function Started ? No;
-// window.onscroll = function () {
-//     if (window.scrollY >= statsContainer.offsetTop) {
-//         if (!started) {
-//             spansStats.forEach((num) => startCount(num));
-//         }
-//         started = true;
-//     }
-// };
 
 function startCount(el) {
     let goal = el.dataset.goal;
@@ -30,5 +21,34 @@ function startCount(el) {
         if (el.textContent == goal) {
             clearInterval(count);
         }
-    }, 2000 / goal);
+    }, 1000 / goal);
 }
+
+//Price
+window.onscroll = function () {
+    if (window.scrollY >= sectionPrice.offsetTop) {
+        percentSpans.forEach((span) => {
+            span.style.width = span.dataset.width;
+        });
+    }
+};
+
+// Counter Section => Requests
+let countDownDate = new Date("Augst 30, 2023 23:59:59").getTime();
+let count = setInterval(() => {
+    let datenew = new Date();
+    let difference = countDownDate - datenew;
+    //  unite
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60 * 60 * 24)) / 1000);
+
+    document.querySelector(".average .day").innerHTML = days < 10 ? `0${days}` : days;
+    document.querySelector(".average .hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+    document.querySelector(".average .minutes").innerHTML = minutes < 10 ? `${minutes} ` : minutes;
+    document.querySelector(".average .seconds").innerHTML = seconds < 10 ? `${minutes}` : seconds;
+    if (difference <= 0) {
+        clearInterval(count);
+    }
+}, 1000);
